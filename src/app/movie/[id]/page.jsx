b7198725c3c103e7dd42259f1a0ae8c9dc7a975d
aryallaxman99 <aryallaxman99@gmail.com>
2024-11-28 02:04:18 +0545

    working on movies page

--- src/app/movie/[id]/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { use } from "react";
-
-const page = ({ params }) => {
-  const { id } = use(params);
-  return <p>ID: {id}</p>;
-};
-
-export default page;
---
+++ src/app/movie/[id]/page.tsx
@@ -0,0 +1,5 @@
+const page = ({ params }: any) => {
+  return <p>ID: {params.id}</p>;
+};
+
+export default page;
